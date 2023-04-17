d3098348522a2bb3b756f108a7363be3d708828d
Joel Doctor <joeldoctor47@gmail.com>
2023-04-17 13:14:37 +0800

    removed testing functions and code

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -11,29 +11,12 @@ const Game = () => {
 
   //Choice Storage
   useEffect(() => {
-    const storedChoices = localStorage.getItem("choices");
-    if (storedChoices) {
-      setChoices(JSON.parse(storedChoices));
-    }
-
     const loadData = JSON.parse(localStorage.getItem("saveData"));
     if (loadData) {
       setSaveData(loadData);
       setDay(loadData.day);
     }
   }, []);
-
-  const handleChoice = (choice) => {
-    const updatedChoices = [...choices, choice];
-    setChoices(updatedChoices);
-    localStorage.setItem("choices", JSON.stringify(updatedChoices));
-    // Perform any logic to determine the outcome of the game based on the user's choices
-  };
-
-  const handleClearStorage = () => {
-    localStorage.removeItem("choices");
-    setChoices([]);
-  };
 
   const storeChoices = (currentDay, currentScene, currentResult) => {
     const currentSave = {
@@ -52,20 +35,7 @@ const Game = () => {
 
   return (
     <div>
-      {choices.length > 0 && (
-        <p>
-          Your previous choices:{" "}
-          {choices.map((choice, index) => (
-            <span key={index}>{choice} </span>
-          ))}
-        </p>
-      )}
-      <p>Choose your fate:</p>
-      <button onClick={() => handleChoice("Yes")}>Yes</button>
-      <button onClick={() => handleChoice("No")}>No</button>
-      {choices.length > 0 && (
-        <button onClick={handleClearStorage}>Clear Choices</button>
-      )}
+      <p>{dayData.date}</p>
       <Scene
         scenes={scenes}
         day={day}
