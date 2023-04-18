7f99d5594db76190962a26b4121e3b02ba92e7c3
kingwell47 <joeldoctor47@gmail.com>
2023-04-18 16:51:22 +0800

    added background

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <Game />
+      <footer>
+        <p>One Earth, One Chance v1.0</p>
+        <p>2023, Joel P. Doctor</p>
+      </footer>
     </div>
   );
 }
