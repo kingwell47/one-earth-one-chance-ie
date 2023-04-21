9e8df1bb20a4bb7f22d241310c2daaca01fa1e08
kingwell47 <joeldoctor47@gmail.com>
2023-04-22 00:03:40 +0800

    added fake loading after each choice

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -15,6 +15,7 @@ const Scene = ({
   const [currentSceneId, setCurrentSceneId] = useState(1);
   const [resultText, setResultText] = useState("");
   const [content, setContent] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const { text, choices } = scenes[currentSceneId];
 
@@ -26,6 +27,14 @@ const Scene = ({
       setResultText(result);
     }
   }, []);
+
+  function startLoading() {
+    setIsLoading(true);
+    setTimeout(function () {
+      // Code to be executed after a 1 second delay
+      setIsLoading(false);
+    }, 250);
+  }
 
   let currentContent = [...text];
 
@@ -47,12 +56,14 @@ const Scene = ({
     setCalamity(newCalamity);
     //store choices
     storeChoices(day, sceneId, result, newCalamity);
+    startLoading();
   };
 
   const handleNextDay = () => {
     setDay(day + 1);
     setCurrentSceneId(1);
     setResultText("");
+    startLoading();
   };
 
   const hoverAnimate = {
@@ -63,43 +74,51 @@ const Scene = ({
   return (
     <>
       <AudioPlayer />
-      {content.map((line, index) => (
-        <motion.p variants={child} key={index} className="sceneText">
-          {line}
-        </motion.p>
-      ))}
-      <motion.fieldset variants={child} id="choiceButtons">
-        {choices ? (
-          choices.map((choice, index) => (
-            <motion.button
-              variants={child}
-              whileHover={hoverAnimate}
-              whileTap={{ scale: 0.9 }}
-              key={index}
-              className="choiceButton"
-              onClick={() =>
-                handleClick(
-                  choice.nextScene,
-                  choice.result,
-                  choice.calamityModifier
-                )
-              }
-            >
-              {choice.text}
-            </motion.button>
-          ))
-        ) : (
-          <motion.button
-            variants={child}
-            whileHover={hoverAnimate}
-            whileTap={{ scale: 0.9 }}
-            onClick={() => handleNextDay()}
-            id="nextDayButton"
-          >
-            {day === 4 ? "You had one chance..." : "Proceed to the next day"}
-          </motion.button>
-        )}
-      </motion.fieldset>
+      {isLoading ? (
+        <></>
+      ) : (
+        <>
+          {content.map((line, index) => (
+            <motion.p variants={child} key={index} className="sceneText">
+              {line}
+            </motion.p>
+          ))}
+          <motion.fieldset variants={child} id="choiceButtons">
+            {choices ? (
+              choices.map((choice, index) => (
+                <motion.button
+                  variants={child}
+                  whileHover={hoverAnimate}
+                  whileTap={{ scale: 0.9 }}
+                  key={index}
+                  className="choiceButton"
+                  onClick={() =>
+                    handleClick(
+                      choice.nextScene,
+                      choice.result,
+                      choice.calamityModifier
+                    )
+                  }
+                >
+                  {choice.text}
+                </motion.button>
+              ))
+            ) : (
+              <motion.button
+                variants={child}
+                whileHover={hoverAnimate}
+                whileTap={{ scale: 0.9 }}
+                onClick={() => handleNextDay()}
+                id="nextDayButton"
+              >
+                {day === 4
+                  ? "You had one chance..."
+                  : "Proceed to the next day"}
+              </motion.button>
+            )}
+          </motion.fieldset>
+        </>
+      )}
     </>
   );
 };
